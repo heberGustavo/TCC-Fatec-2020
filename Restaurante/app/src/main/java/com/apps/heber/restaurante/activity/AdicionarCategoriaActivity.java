42f6ca957b6a90d3985a8df5f8d6164db16b0339
Thiago <thiagodessa41@gmail.com>
2019-09-06 17:09:05 -0300

    Adicionou layouts CategoriaActivity e MesaActivity

---
+++ Restaurante/app/src/main/java/com/apps/heber/restaurante/activity/AdicionarCategoriaActivity.java
@@ -0,0 +1,15 @@
+package com.apps.heber.restaurante.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.heber.restaurante.R;
+
+public class AdicionarCategoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_categoria);
+    }
+}
