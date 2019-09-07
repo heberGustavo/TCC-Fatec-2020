8c77fa3d408cf8c2742f46c9f7ec5ca204c95f99
Heber Gustavo <heber.gbarbosa@hotmail.com>
2019-09-07 14:57:56 -0300

    Adicionando menu salvar em AdicionarNovoCardapio.java

--- Restaurante/app/src/main/java/com/apps/heber/restaurante/activity/AdicionarNovoCardapioActivity.java
+++ Restaurante/app/src/main/java/com/apps/heber/restaurante/activity/AdicionarNovoCardapioActivity.java
@@ -3,7 +3,10 @@ package com.apps.heber.restaurante.activity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.apps.heber.restaurante.R;
 
@@ -21,5 +24,24 @@ public class AdicionarNovoCardapioActivity extends AppCompatActivity {
         actionBar.setTitle("Novo Cardapio");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_salvar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_salvar:
+                Toast.makeText(getApplicationContext(),
+                        "Menu salvar",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
